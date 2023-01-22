f8933f36497d31bbb0fb04ac76d62eab84fcee32
Nike Merkushev <nikemerkushev@MacBook-Air-M1-Nike.local>
2023-01-23 02:01:13 +0300

    start work with city project

---
+++ work with teacher/js/my-code.js
@@ -0,0 +1,32 @@
+// let startButton = document.querySelector('.container .start-button');
+
+// let question = rompt('Напишите город с маленькой буквы');
+
+// function checkHaveACity(inputCity) {
+// 	let result;
+// 	for (let elem of cities) {
+// 		if (elem == inputCity) {
+// 			result = getAnswer(inputCity);
+// 		}
+// 	}
+// 	console.log(result);
+// }
+
+// checkHaveACity(variable);
+
+let array = ['ваня', 'яша', 'амур', 'рык', 'кедр']; //
+
+function getNames() {
+	return rompt('впиши  имя');
+}
+
+function fun(arr, names) {
+	for(let elem of arr) {
+		let variable = elem.split('');
+		
+	}
+}
+
+let result = fun(array, getNames());
+console.log(result); 
+
