013d1c42bd084e13192caff2876ceec3d4569ec2
Nike <nikegame2275@mail.ru>
2023-05-04 17:15:02 +0300

    students-table

--- student-table/js/code.js
+++ student-table/js/code.js
@@ -1,80 +1,69 @@
-let arrayObject = [
-	{
-		'name': 'Bike',
-		'surname': 'Aerk',
-		'patronymic': 'Zadic',
-		'birthday': '30.10.1990',
-		'start': 11,
-		'faculty': 'farbis',
-	},
-
-	{
-		'name': 'Qike',
-		'surname': 'Kerk',
-		'patronymic': 'Badic',
-		'birthday': '30.10.1999',
-		'start': 7,
-		'faculty': 'qarbis',
-	},
-
-	{
-		'name': 'Sike',
-		'surname': 'Perk',
-		'patronymic': 'Adic',
-		'birthday': '30.10.2001',
-		'start': 25,
-		'faculty': 'gharbis',
-	},
-	
-	{
-		'name': 'Aike',
-		'surname': 'Zerk',
-		'patronymic': 'Padic',
-		'birthday': '30.10.995',
-		'start': 1,
-		'faculty': 'varbis',
-	},
-];
+let arrayObject = [];
 
 const formButton = document.getElementById('form-button');
 const takeElementsTable = createItemFromBodyTable;
 const tableBody = document.querySelector('.container .table .table-body');
-const buttonName = document.getElementById('button-name');
-const buttonSurName = document.getElementById('button-surname');
-const buttonPatronymic = document.getElementById('button-patronymic');
 const buttonBth = document.getElementById('button-bthday');
-const buttonStart = document.getElementById('button-start-day');
-const buttonFaculty = document.getElementById('button-faculty');
 const allTableButtons = document.querySelectorAll('.table-button');
 let path;
 
+
+
+// СОБЫТИЕ на кнопку form 
 formButton.onclick = (e) => {
-	if (checkInputValue()) { // проверка на наличие ввода всех input
+
+	// проверка на наличие ввода всех input
+	if (checkInputValue()) {                
 		e.preventDefault();
-		let newObj = createNewObject(); // создание объекта и добавление его в массив
+
+		// создание объекта и добавление его в массив
+		let newObj = createNewObject();  
+		
+		// добавление елементов таблицы
 		tableBody.append(takeElementsTable(newObj));
+
+		// удаление (очищение) всех введенных input
+		deleteAllInputValue();
 	}
 }
 
+// СОБЫТИЕ на сортировочнце кнопки table
 allTableButtons.forEach((buttonBth) => {
 	buttonBth.onclick = (e) => {
-		path = e.currentTarget.dataset.path;
-		if (path === 'birthday') sortData();
-		arrayObject.sort((a, b) => {
-			if (a[path] > b[path]) return 1;
-			else return -1;
-		});
-		console.log(arrayObject);
+		// удаляем данные таблицы
+		deleteAllTableElements();    
+
+		// сохраняем имя класса нажатой кнопки
+		path = e.currentTarget.dataset.path; 
+
+		// сортируем массив по заданн сдассу
+		arrayObject.sort(sortFunction());    
+
+		// выгружаем отсортированныйй массив по классу
+		arrayObject.map((obj => {tableBody.append(takeElementsTable(obj))}))
 	}
 });
 
-function sortData() {
-	arrayObject.sort((a, b) => {
-		console.log(Date.parse(a.birthday))
-		console.log(a.birthday);
-	})
+
+
+
+
+
+// Ф-ция сортировки массива объектов
+function sortFunction() {
+	return function(one, two) {
+		if (one[path] > two[path]) return 1;
+		else return -1;
+	}
 }
 
+// Ф-ция удаления елементов таблицы
+function deleteAllTableElements() {
+	let allElementTable = document.querySelectorAll('.tr');
+	allElementTable.forEach(elem => elem.remove());
+}
+
+// Ф-ция проверки валид input
 function checkInputValue() {
 	let allInput = document.querySelectorAll('input');
 	let result = true;
@@ -84,17 +73,12 @@ function checkInputValue() {
 	return result;
 }
 
-function createNewObject() {
-	let newObject = {
-		'name': document.getElementById('input-name').value,
-		'surname': document.getElementById('input-surname').value,
-		'patronymic': document.getElementById('input-patronymic').value,
-		'birthday': document.getElementById('input-bth-day').value,
-		'start': parseInt(document.getElementById('input-start').value),
-		'faculty': document.getElementById('input-faculty').value,
-	};
-	arrayObject.push(newObject);
-	return newObject;
+// Ф-ция удаления input.value
+function deleteAllInputValue() {
+	let allInput = document.querySelectorAll('input');
+	allInput.forEach((input) => {input.value = '';});
 }
+
+
 
 
--- student-table/js/create.js
+++ student-table/js/create.js
@@ -1,8 +1,45 @@
 
 const $container = document.querySelector('.container');
 
+function correctDate(num) {
+	if (num >= 0 && num <= 9) return '0' + num;
+	else return num;
+}
+
+function validInputValue(value) {
+	return value[0].toUpperCase() + value.slice(1).toLowerCase();
+}
+
+function createNewObject() {
+	let newObject = {
+		'name': validInputValue(document.getElementById('input-name').value),
+		'surname': validInputValue(document.getElementById('input-surname').value),
+		'patronymic': validInputValue(document.getElementById('input-patronymic').value),
+		'birthday': Date.parse(document.getElementById('input-bth-day').value),
+		'start': parseInt(document.getElementById('input-start').value),
+		'faculty': validInputValue(document.getElementById('input-faculty').value),
+	};
+	arrayObject.push(newObject);
+	return newObject;
+}
+
 function createForm() {
 	let form = document.createElement('form');
+
+	let blockInputName = document.createElement('div');
+	let blockInputSurname = document.createElement('div');
+	let blockInputPatronymic = document.createElement('div');
+	let blockInputBthDay = document.createElement('div');
+	let blockInputStart = document.createElement('div');
+	let blockInputFaculty = document.createElement('div');
+
+	let labelForInputName = document.createElement('label');
+	let labelForInputSurname = document.createElement('label');
+	let labelForInputPatronymic = document.createElement('label');
+	let labelForInputBthDay = document.createElement('label');
+	let labelForInputStart = document.createElement('label');
+	let labelForInputFaculty = document.createElement('label');
+
 	let inputName = document.createElement('input');
 	let	inputSurname = document.createElement('input');
 	let inputPatronymic = document.createElement('input');
@@ -11,34 +48,66 @@ function createForm() {
 	let inputFaculty = document.createElement('input');
 	let formButton = document.createElement('button');
 
-	inputStart.type = Number
+	form.classList.add('form', 'form-button');
 
-	form.classList.add('form');
+	blockInputName.classList.add('form__blok', 'block-input-name');
+	blockInputSurname.classList.add('form__blok', 'block-input-surname');
+	blockInputPatronymic.classList.add('form__blok', 'block-input-patronymic');
+	blockInputBthDay.classList.add('form__blok', 'block-input-birthday')
+	blockInputStart.classList.add('form__blok', 'block-input-start')
+	blockInputFaculty.classList.add('form__blok', 'block-input-faculty')
+
+	labelForInputName.classList.add('form__label', 'label')
+	labelForInputSurname.classList.add('form__label', 'label')	
+	labelForInputPatronymic.classList.add('form__label', 'label')
+	labelForInputBthDay.classList.add('form__label', 'label')
+	labelForInputStart.classList.add('form__label', 'label')
+	labelForInputFaculty.classList.add('form__label', 'label')
+
 	inputName.classList.add('input-name', 'input');
-	inputName.id = 'input-name';
 	inputSurname.classList.add('input-surname', 'input');
-	inputSurname.id = 'input-surname';
 	inputPatronymic.classList.add('input-patronymic', 'input');
-	inputPatronymic.id = 'input-patronymic';
 	inputBthDay.classList.add('input-bth-day', 'input');
-	inputBthDay.id = 'input-bth-day';
 	inputStart.classList.add('input-start', 'input');
-	inputStart.id = 'input-start';
 	inputFaculty.classList.add('input-faculty', 'input');
+
+	inputName.id = 'input-name';
+	inputSurname.id = 'input-surname';
+	inputPatronymic.id = 'input-patronymic';
+	inputBthDay.id = 'input-bth-day';
+	inputStart.id = 'input-start';
 	inputFaculty.id = 'input-faculty';
-	formButton.classList.add('form-button');
 	formButton.id = 'form-button';
 
+	inputStart.type = Number
 	inputBthDay.type = 'date';
-	inputStart.type = 'number';
+	inputStart.type = 'date';
+
+	inputName.setAttribute('name', 'name');
+	inputSurname.setAttribute('name', 'surname');
+	inputPatronymic.setAttribute('name', 'patronymic');
+	inputBthDay.setAttribute('name', 'birthday');
+	inputStart.setAttribute('name', 'start');
+	inputFaculty.setAttribute('name', 'faculty');
+
+	labelForInputName.setAttribute('for', 'name');
+	labelForInputSurname.setAttribute('for', 'surname');
+	labelForInputPatronymic.setAttribute('for', 'patronymic');
+	labelForInputBthDay.setAttribute('for', 'birthday');
+	labelForInputStart.setAttribute('for', 'start');
+	labelForInputFaculty.setAttribute('for', 'faculty');
+
+	inputBthDay.setAttribute("min", "2018-01-01");
+	inputStart.setAttribute("min", "2000-01-01");
 
 	formButton.innerHTML = 'add student';
-	inputName.placeholder = 'Введите имя';
-	inputSurname.placeholder = 'Введите фамилию';
-	inputPatronymic.placeholder = 'Введите отчество';
-	inputBthDay.placeholder = 'День рождения';
-	inputStart.placeholder = 'Начало обучения';
-	inputFaculty.placeholder = 'Факультет';
+
+	labelForInputName.textContent = 'Введите имя';
+	labelForInputSurname.textContent = 'Введите фамилию';
+	labelForInputPatronymic.textContent = 'Введите отчество';
+	labelForInputBthDay.textContent = 'День рождения';
+	labelForInputStart.textContent = 'Начало обучения';
+	labelForInputFaculty.textContent = 'Факультет';
 
 	inputName.required = true;
 	inputSurname.required = true;
@@ -47,23 +116,28 @@ function createForm() {
 	inputStart.required = true;
 	inputFaculty.required = true;
 
+	blockInputName.append(labelForInputName, inputName);
+	blockInputSurname.append(labelForInputSurname, inputSurname);
+	blockInputPatronymic.append(labelForInputPatronymic, inputPatronymic);
+	blockInputBthDay.append(labelForInputBthDay, inputBthDay);
+	blockInputStart.append(labelForInputStart, inputStart);
+	blockInputFaculty.append(labelForInputFaculty, inputFaculty);
+
 	form.append (
-		inputName,
-		inputSurname,
-		inputPatronymic,
-		inputBthDay,
-		inputStart,
-		inputFaculty,
+		blockInputName,
+		blockInputSurname,
+		blockInputPatronymic,
+		blockInputBthDay,
+		blockInputStart,
+		blockInputFaculty,
 		formButton,
 	);
 	
-	return {
-		form
-	}
+	return form, formButton
+
 }
 
 function createItemFromBodyTable(obj) {
-
 	let tr = document.createElement('tr');
 	let tdItemName = document.createElement('td');
 	let tdItemSurName = document.createElement('td');
@@ -72,6 +146,7 @@ function createItemFromBodyTable(obj) {
 	let tdItemStart = document.createElement('td');
 	let tdItemFaculty = document.createElement('td');
 
+	tr.classList.add('tr');
 	tdItemName.classList.add('table__item', 'item-name');
 	tdItemSurName.classList.add('table__item', 'item-surname');
 	tdItemPatronymic.classList.add('table__item', 'item-patronymic');
@@ -79,10 +154,14 @@ function createItemFromBodyTable(obj) {
 	tdItemStart.classList.add('table__item', 'item-start');
 	tdItemFaculty.classList.add('table__item', 'item-faculty');
 
+	
+	let newDate = new Date(obj.birthday);
+	let resultDate = newDate.toISOString().substr(0, 10);
+
 	tdItemName.textContent = obj.name;
 	tdItemSurName.textContent = obj.surname;
 	tdItemPatronymic.textContent = obj.patronymic;
-	tdItemBthDay.textContent = obj.birthday;
+	tdItemBthDay.textContent = resultDate;
 	tdItemStart.textContent = obj.start;
 	tdItemFaculty.textContent = obj.faculty;
 
@@ -90,4 +169,6 @@ function createItemFromBodyTable(obj) {
 	
 	return tr;
 }
+
 $container.append(createForm().form);
+
